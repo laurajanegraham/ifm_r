92c64c3c7d0c8489a432ab80bb41e3bcecb18c3e
lorra <lgxlg3@nottingham.ac.uk>
2015-11-27 15:11:07 -0500

    add in the main run script

---
+++ run.ifm.R
@@ -0,0 +1,10 @@
+# load all functions
+source("R/ds.factors.R")
+source("R/ds.run.R")
+source("R/ifm.sim.R")
+source("R/model.fit.R")
+
+# run ifm sim and record run time
+strt<-Sys.time()
+ifm.sim(input[,1], 200, 500, 8, input[,2])
+print(Sys.time()-strt)
